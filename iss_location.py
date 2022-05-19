c725169cd2e2bc7103486bd85189351fc0fefbfa
coreydjordan <83131355+coreydjordan@users.noreply.github.com>
2022-05-19 13:17:24 -0500

    added location request

--- iss_location.py
+++ iss_location.py
@@ -8,5 +8,21 @@ query = {
     "lon": '180'
 }
 
+
+
 response = requests.get(ENV["URL"], params=query)
-print(response.json())
+# print(response.json())
+
+latitude = response.json()['iss_position']['latitude']
+longitude = response.json()['iss_position']['longitude']
+
+loc_query = {
+    "key": ENV["ACCESS_TOKEN"],
+    "lon": longitude,
+    "lat": latitude,
+    "format": "json"
+}
+
+loc_res = requests.get(ENV["LOCATION_URL"], params=loc_query)
+print(loc_res.json())
+# print(longitude,latitude)
